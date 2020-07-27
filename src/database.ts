7150e533ce6e00b63a7a16e20c55d9ef606e6710
ostrebler <strebler.olivier@hotmail.fr>
2020-07-27 12:00:55 +0200

    Update

--- src/database.ts
+++ src/database.ts
@@ -6,19 +6,21 @@ export class Database {
   handle: Promise<Db>;
   collections: Record<string, Collection<any>>;
 
-  constructor(url: string, databaseName: string) {
-    this.name = databaseName;
+  constructor(url: string, name: string) {
+    this.name = name;
     this.handle = MongoClient.connect(url, {
       useUnifiedTopology: true
-    }).then(client => client.db(databaseName));
+    }).then(client => client.db(name));
     this.collections = Object.create(null);
   }
 
-  collection<TSchema extends object>(name: string, config?: CollectionConfig) {
+  collection<TSchema extends object>(
+    name: string,
+    config?: CollectionConfig
+  ): Collection<TSchema> {
     if (!(name in this.collections))
       this.collections[name] = new Collection<TSchema>(this, name, config);
-    const collection: Collection<TSchema> = this.collections[name];
-    return collection;
+    return this.collections[name];
   }
 
   dropDatabase() {
@@ -26,10 +28,11 @@ export class Database {
   }
 
   _cascade(name: string, deletedKeys: Array<any>) {
-    const cascades = Object.values(this.collections).map(collection =>
-      collection._cascade(name, deletedKeys)
-    );
-    return Promise.all(cascades)
+    return Promise.all(
+      Object.values(this.collections).map(collection =>
+        collection._cascade(name, deletedKeys)
+      )
+    )
       .then(actions => flatten(actions))
       .then(actions => Promise.all(actions.map(action => action())));
   }
